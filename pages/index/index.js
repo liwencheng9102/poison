6c1715064aee06c9e974d6e83dc33630570e56e1
poison-miss <1609306134@qq.com>
2020-12-09 22:13:02 +0800

    删除文件夹

---
+++ pages/index/index.js
@@ -0,0 +1,132 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgs: [
+      "../../image/goods01.jpg",
+      "../../image/goods02.jpg",
+      "../../image/goods03.jpg",
+      "../../image/goods04.jpg"
+    ],
+    iconArray: [{
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "签到"
+      },
+      {
+        iconUrl: "../../image/icon-fujin.png",
+        iconText: "地址"
+      },
+    ],
+    goodsList: [{
+        id: 1,
+        goodsName: "商品名称",
+        goodsImage: "../../image/goods01.jpg",
+        goodsAddress: "广州",
+        goodsPrice: "111.00"
+      },
+      {
+        id: 1,
+        goodsName: "商品名称",
+        goodsImage: "../../image/goods01.jpg",
+        goodsAddress: "广州",
+        goodsPrice: "111.00"
+      },
+      {
+        id: 1,
+        goodsName: "商品名称",
+        goodsImage: "../../image/goods01.jpg",
+        goodsAddress: "广州",
+        goodsPrice: "111.00"
+      },
+      {
+        id: 1,
+        goodsName: "商品名称",
+        goodsImage: "../../image/goods01.jpg",
+        goodsAddress: "广州",
+        goodsPrice: "111.00"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
